bfc2ba37d05f4eba985303a9658534af69006661
MKhodagholi <mdkhodagholi@gmail.com>
2023-01-01 21:54:09 +0330

    2. add color (primary, secondary, thrid, forth, blacks, gray)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      primaryColor: '#576BF0',
+      secondaryColor: '#F5AF2B',
+      thirdColor: '#17268A',
+      forthColor: '#352556',
+      black: {
+        400: '#363636',
+        500: '2A2A2A',
+        700: '#1C1E23',
+        800: '292929',
+        900: '#000000',
+      },
+      gray: {
+        700: '#ECEBEB',
+      },
+    },
   },
   plugins: [],
 };
