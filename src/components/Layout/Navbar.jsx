3552578bbd25f990ed45fdbeb81bcfa5fdb16f10
MKhodagholi <mdkhodagholi@gmail.com>
2023-01-05 00:08:47 +0330

    8. add Iran Yekan font & complete header navbar

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -17,26 +17,39 @@ export default function Navbar() {
   ];
 
   return (
-    <header className="h-[100px] flex items-center px-5">
-      <h1>کارآموزش</h1>
-      <nav>
-        <ul className="flex">
-          {links.map((link) => (
-            <li
-              key={link.name}
-              className="w-[170px] py-2 ml-4 text-center rounded-3xl bg-primaryColor text-white"
-            >
-              <Link to={{ pathname: link.href }}>
-                {link.name}
-              </Link>
+    <header className="h-[100px] flex justify-between items-center px-5">
+      <div className="flex items-center">
+        <h1 className="ml-4 text-2xl text-primaryColor">
+          کارآموزش
+        </h1>
+        <nav>
+          <ul className="flex">
+            {links.map((link) => (
+              <li key={link.name}>
+                <Link
+                  to={{ pathname: link.href }}
+                  className="block w-[170px] py-2 ml-4 text-center text-sm rounded-3xl bg-primaryColor text-white"
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+            <li>
+              <div className="block w-[170px] py-2 ml-4 text-center rounded-3xl bg-primaryColor text-white">
+                ساخت رزومه
+              </div>
             </li>
-          ))}
-          <li className="w-[170px] py-2 ml-4 text-center rounded-3xl bg-primaryColor text-white">
-            <div></div>
-          </li>
-        </ul>
-      </nav>
-      <div></div>
+          </ul>
+        </nav>
+      </div>
+      <div className="flex justify-between items-center w-[270px] h-[50px] rounded-3xl bg-gray-700 shadow-md">
+        <Link className="flex justify-center items-center w-[100px] h-full text-black-900">
+          ورود
+        </Link>
+        <Link className="flex justify-center items-center w-[170px] h-full bg-primaryColor text-white rounded-3xl">
+          عضویت
+        </Link>
+      </div>
     </header>
   );
 }
