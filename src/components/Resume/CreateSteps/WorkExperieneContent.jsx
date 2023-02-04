77039d1392d121d9deb529dc08275c9ce87ffd44
MKhodagholi <mdkhodagholi@gmail.com>
2023-02-04 20:58:40 +0330

    add onChange functions for WorkExperienceContent Component

--- src/components/Resume/CreateSteps/WorkExperieneContent.jsx
+++ src/components/Resume/CreateSteps/WorkExperieneContent.jsx
@@ -1,6 +1,98 @@
+import { useRef, useEffect } from 'react';
+import {
+  useDispatch,
+  useSelector,
+} from 'react-redux';
+
 import ResumeInput from '../ResumeInput';
+import { resumeActions } from '../../../store/resume-slice';
 
 const WorkExperienceContent = () => {
+  const dispatch = useDispatch();
+
+  // initialize
+  const { workExperience } = useSelector(
+    (state) => state.resume,
+  );
+
+  const {
+    employmentStatus,
+    employmentTitle,
+    occupationalGroup,
+    companyName,
+    startDate,
+    endDate,
+  } = workExperience;
+
+  const employmentStatusRef = useRef(null);
+  const employmentTitleRef = useRef(null);
+  const occupationalGroupRef = useRef(null);
+  const companyNameRef = useRef(null);
+  const startDateRef = useRef(null);
+  const endDateRef = useRef(null);
+
+  useEffect(() => {
+    employmentStatusRef.current.value =
+      employmentStatus;
+    employmentTitleRef.current.value =
+      employmentTitle;
+    startDateRef.current.value = startDate;
+    endDateRef.current.value = endDate;
+    occupationalGroupRef.current.value =
+      occupationalGroup;
+    companyNameRef.current.value = companyName;
+  }, []);
+
+  const employmentStatusChangeHandler = (e) => {
+    const employmentStatus_value = e.target.value;
+
+    dispatch(
+      resumeActions.changeWorkExperience({
+        prop: 'employmentStatus',
+        value: employmentStatus_value,
+      }),
+    );
+  };
+
+  const employmentTitleChangeHandler = (e) => {
+    const employmentTitle_value = e.target.value;
+
+    dispatch(
+      resumeActions.changeWorkExperience({
+        prop: 'employmentTitle',
+        value: employmentTitle_value,
+      }),
+    );
+  };
+
+  const occupationalGroupChangeHandler = (e) => {
+    const occupationalGroup_value =
+      e.target.value;
+
+    dispatch(
+      resumeActions.changeWorkExperience({
+        prop: 'occupationalGroup',
+        value: occupationalGroup_value,
+      }),
+    );
+  };
+
+  const companyNameChangeHandler = (e) => {
+    const companyNameChange_value =
+      e.target.value;
+
+    dispatch(
+      resumeActions.changeWorkExperience({
+        prop: 'companyNameChange',
+        value: companyNameChange_value,
+      }),
+    );
+  };
+
+  const startDateChangeHandler = (e) => {};
+
+  const endDateChangeHandler = (e) => {};
+
   return (
     <div className="resume-step-content">
       <div className="grid grid-cols-2 gap-20 pt-8">
@@ -8,13 +100,17 @@ const WorkExperienceContent = () => {
           label="وضعیت شغلی"
           type="text"
           name="employment-status"
-          onChange={null}
+          onChange={employmentStatusChangeHandler}
+          placeholder="بازنشسته"
+          innerRef={employmentStatusRef}
         />
         <ResumeInput
           label="عنوان شغلی"
           type="text"
           name="employment-title"
-          onChange={null}
+          onChange={employmentTitleChangeHandler}
+          placeholder="جونیور"
+          innerRef={employmentTitleRef}
         />
       </div>
       <div className="grid grid-cols-2 gap-20 pt-8">
@@ -22,13 +118,19 @@ const WorkExperienceContent = () => {
           label="گروه شغلی"
           type="text"
           name="occupational-group"
-          onChange={null}
+          onChange={
+            occupationalGroupChangeHandler
+          }
+          placeholder="بیکاران شماره B"
+          innerRef={occupationalGroupRef}
         />
         <ResumeInput
           label="نام شرکت"
           type="text"
           name="company-name"
-          onChange={null}
+          onChange={companyNameChangeHandler}
+          placeholder="اسنپ"
+          innerRef={companyNameRef}
         />
       </div>
       <div className="grid grid-cols-2 gap-20 pt-8">
@@ -37,13 +139,15 @@ const WorkExperienceContent = () => {
             label="تاریخ شروع"
             type="date"
             name="start-date-work"
-            onChange={null}
+            onChange={startDateChangeHandler}
+            innerRef={startDateRef}
           />
           <ResumeInput
             label="تاریخ پایان"
             type="date"
             name="end-date-work"
-            onChange={null}
+            onChange={endDateChangeHandler}
+            innerRef={endDateRef}
           />
         </div>
       </div>
