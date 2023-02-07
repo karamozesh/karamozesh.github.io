93e66e5d7e4bbe6d42b966fe06d43087eb293cdc
MKhodagholi <mdkhodagholi@gmail.com>
2023-02-07 14:26:55 +0330

    add base for vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+  base: '/vite-deploy-demo/',
   server: {
     port: 3000,
   },
