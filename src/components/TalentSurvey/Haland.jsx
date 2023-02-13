01f157b479828bff128e9749b1a1c34db47b44a0
MKhodagholi <mdkhodagholi@gmail.com>
2023-02-13 13:55:01 +0330

    add haland-slice & change struct of data object & impelment test

--- src/components/TalentSurvey/Haland.jsx
+++ src/components/TalentSurvey/Haland.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { data } from '../../Data/data';
+import { questions_holland } from '../../Data/data';
 import { useState } from 'react';
+import QuestionCard from './QuestionCard';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 function Haland() {
+  const { totalQuestions, ansArray } =
+    useSelector((state) => state.haland);
   const [currentPage, setCurrentPage] =
     useState(1);
-  console.log(data);
 
   const numQuestions = 6;
   const startIndex =
@@ -13,10 +17,8 @@ function Haland() {
 
   const endIndex = currentPage * numQuestions;
 
-  const currentQuestions = data.slice(
-    startIndex,
-    endIndex,
-  );
+  const currentQuestions =
+    questions_holland.slice(startIndex, endIndex);
 
   const backHandler = () => {
     setCurrentPage((prev) => prev - 1);
@@ -25,55 +27,34 @@ function Haland() {
   const prevHandler = () => {
     setCurrentPage((prev) => prev + 1);
   };
+
+  console.log(ansArray);
+
+  const isLastPage =
+    currentPage ===
+    Math.floor(totalQuestions / 6);
+
   return (
     <section className="container mx-auto p-7 mt-7">
       <section className="flex flex-col items-center justify-center gap-5">
         <section className="border shadow-lg shadow-blue-500/50 grid  grid-rows-3 grid-cols-2 gap-10 justify-items-center p-10 content-center rounded-3xl">
-          {currentQuestions.length > 2 &&
+          {currentQuestions.length > 0 &&
             currentQuestions.map(
-              (question, index) => {
-                return (
-                  <>
-                    <div>
-                      <p>{question.question}</p>
-
-                      <div className="grid grid-cols-5 mb-10 grid-rows-1 gap-2 mt-5 text-md text-xs">
-                        <button className=" rounded-xl border shadow-lg">
-                          {question.answer1.label}
-                        </button>
-                        <button className=" rounded-xl border shadow-lg">
-                          {question.answer2.label}
-                        </button>
-                        <button className=" rounded-xl border shadow-lg">
-                          {question.answer3.label}
-                        </button>
-                        <button className=" rounded-xl border shadow-lg">
-                          {question.answer4.label}
-                        </button>
-
-                        <div className="relative rounded-xl border shadow-lg">
-                          <label
-                            htmlFor=""
-                            className="absolute"
-                          >
-                            {
-                              question.answer5
-                                .label
-                            }
-                          </label>
-                          <input
-                            type="checkbox"
-                            className="absolute w-full opacity-0"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </>
-                );
-              },
+              (questionObj, index) => (
+                <QuestionCard
+                  key={index}
+                  questionObj={{
+                    ...questionObj,
+                    id:
+                      (currentPage - 1) * 6 +
+                      index +
+                      1,
+                  }}
+                />
+              ),
             )}
         </section>
-        <section className=" flex gap-5">
+        <section className="flex self-end gap-5">
           {currentPage > 1 && (
             <button
               onClick={backHandler}
@@ -83,12 +64,21 @@ function Haland() {
             </button>
           )}
 
-          <button
-            onClick={prevHandler}
-            className="bg-[#F5AF2B] rounded-xl p-2 px-3"
-          >
-            سوالات بعدی
-          </button>
+          {!isLastPage && (
+            <button
+              onClick={prevHandler}
+              className="bg-[#F5AF2B] rounded-xl p-2 px-3"
+            >
+              سوالات بعدی
+            </button>
+          )}
+          {isLastPage && (
+            <button className="py-3 px-6 text-white bg-primaryColor rounded-3xl">
+              <Link to="result">
+                دیدن نتیجه تست
+              </Link>
+            </button>
+          )}
         </section>
       </section>
     </section>
