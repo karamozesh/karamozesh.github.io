e8e744c39b685b225fa2add983cda089db796e28
hediyehafzalzadeh <hedye.afzalzade@gmail.com>
2023-07-13 15:40:09 +0200

    final fixing

--- src/pages/Skill/UI/SkillRoad.jsx
+++ src/pages/Skill/UI/SkillRoad.jsx
@@ -1,11 +1,12 @@
 
+import Button from '../../../components/UI/Button'
 
 export default function SkillRoad ({title , href }){
 
     return(
             
-           <button className="bg-[#576BF0] text-white p-2 h-16 rounded-3xl">
+           <Button className="bg-[#576BF0] text-xs font-light leading-4 text-white p-6 h-12 rounded-3xl">
             <a href={href}>{title}</a>
-           </button>
+           </Button>
     )
 }
